47a3b08baed7aef94fbf306fc265b213ebb0f2a8
Erik <anderson8221@student.cptc.edu>
2020-10-20 15:46:12 -0700

    Added form

---
+++ Chuck norris forms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using ChuckNorrisAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chuck_norris_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            
+            string[] Categories = (await ChuckNorrisClient.GetCategories()).ToArray();
+
+            listBox1.Items.AddRange(Categories);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = (await ChuckNorrisClient.GetRandomJoke()).JokeText;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using ChuckNorrisAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.AddRange(GetCategories().ToArray());
+        }
+
+        private IEnumerable<string> GetCategories()
+        {
+            return ChuckNorrisClient.GetCategories().Result;
+        }
+    }
+}
